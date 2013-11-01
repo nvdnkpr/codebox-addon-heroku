0439b13539dce5c538736954e8af4e8b2992175a
Samy Pessé <samypesse@gmail.com>
2013-11-01 12:34:55 +0100

    Add settings in client

--- client.js
+++ client.js
@@ -1,5 +1,21 @@
 define([], function() {
     var commands = require("core/commands");
+    var settings = require("utils/settings");
+
+    // Add settings page
+    settings.add({
+        'namespace': "heroku",
+        'section': "deployment",
+        'title': "Heroku",
+        'fields': {
+            'key': {
+                'label': "Key",
+                'type': "text",
+                "help": "You can find your api key in your settings Heroku.",
+                "default": ""
+            }
+        }
+    });
 
     // Add opening command
     commands.register("deploy.heroku", {
