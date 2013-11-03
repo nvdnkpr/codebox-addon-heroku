604402d3c564e5d162aa4bd3fff3cb743a7683c7
Samy Pessé <samypesse@gmail.com>
2013-11-03 19:14:07 +0100

    Add support for adding key

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 define([], function() {
     var commands = require("core/commands");
     var settings = require("utils/settings");
+    var api = require("core/api");
 
     // Add settings page
     settings.add({
@@ -11,8 +12,17 @@ define([], function() {
             'key': {
                 'label': "Key",
                 'type': "text",
-                "help": "You can find your api key in your settings Heroku.",
-                "default": ""
+                'help': "You can find your api key in your settings Heroku.",
+                'default': ""
+            },
+            'regenerate': {
+                'label': "",
+                'type': "action",
+                'content': "Authorize SSH key on Heroku",
+                'help': "Save your settings before authorizing to Heroku.",
+                'trigger': function() {
+                    return api.rpc("/heroku/update");
+                }
             }
         }
     });
--- node/service.js
+++ node/service.js
@@ -1,7 +1,9 @@
 // Requires
 var Q = require('q');
+var fs = require('fs');
+var path = require('path');
 var _ = require('underscore');
-var heroku = require("heroku");
+var Heroku = require('heroku-client');
 
 function HerokuRPCService(workspace) {
     this.workspace = workspace;
@@ -14,8 +16,8 @@ HerokuRPCService.prototype._client = function(user) {
     if (!user.settings.heroku || !user.settings.heroku.key) {
         return Q.reject(new Error("User need to configure heroku with its api key"));
     }
-    var client = new heroku.Heroku({
-        key: user.settings.heroku.key
+    var client = new Heroku({
+        token: user.settings.heroku.key
     });
     return Q(client)
 };
@@ -23,10 +25,26 @@ HerokuRPCService.prototype._client = function(user) {
 // List user applications
 HerokuRPCService.prototype.apps = function(args, meta) {
     return this._client(meta.user).then(function(client) {
-        return Q.nfcall(client.get_apps);
+        return client.apps().list();
     }).then(function(apps) {
-        console.log(apps);
         return Q(apps);
+    });
+};
+
+// Send key to heroku
+HerokuRPCService.prototype.update = function(args, meta) {
+    var publickey = null;
+    var that = this;
+    var publickey_file = path.join(process.env.HOME, '.ssh/id_rsa.pub');
+    return Q.nfcall(fs.readFile, publickey_file, 'utf8').then(function(content) {
+        publickey = content;
+        return that._client(meta.user);
+    }).then(function(client) {
+        return client.account().keys().create({
+            'public_key': publickey
+        });
+    }).then(function(key) {
+        return Q(key);
     });
 };
 
